46215cc183f5167c020d80dc9010b857d95abc90
alecgalba <alecgalba@gmail.com>
2018-03-02 15:07:13 -0500

    done

--- index.js
+++ index.js
@@ -12,14 +12,64 @@ function bruteForceTwoSum(arr, sum) {
     return pairs
 }
 
-function binarySearchTwoSum() {
-
+function findMinAndRemoveSorted(array){
+  return array.shift()
 }
 
-function binaryMatch() {
+function merge(firstHalf, secondHalf){
+    let sorted = []
+    while(firstHalf.length != 0 && secondHalf.length != 0){
+      if (firstHalf[0] < secondHalf[0]) {
+        sorted.push(findMinAndRemoveSorted(firstHalf))
+      } else {
+        sorted.push(findMinAndRemoveSorted(secondHalf))
+      }
+    }
+    return sorted.concat(firstHalf).concat(secondHalf)
+  }
 
-}
+  function mergeSort(array){
+      let midpoint = array.length/2
+      let firstHalf = array.slice(0, midpoint)
+      let secondHalf = array.slice(midpoint, array.length)
+      let sorted;
 
-function hashTwoSum() {
+      if (array.length < 2){
+        return array
+      } else {
+        sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+      }
+      return sorted
+    }
 
-}
+  function binaryMatch(arr, num) {
+    arr = mergeSort(arr)
+    if (arr.includes(num)) {
+      return true
+    }
+  }
+
+  function binarySearchTwoSum(arr, sum) {
+    let pairs = []
+    arr = mergeSort(arr)
+    for (let i = 0; i < arr.length; i++) {
+      let num = sum - arr[i]
+      if (binaryMatch(arr, num)) {
+        pairs.push([arr[i], num])
+      }
+    }
+    return pairs.slice(0, pairs.length/2)
+  }
+
+  function hashTwoSum(arr, sum) {
+    let pairs = [];
+    for (let i=0; i < arr.length; i++) {
+      let adder = arr[i]
+      let secondAdder = sum - arr[i]
+      let num = {adder: secondAdder}
+      if (arr.includes(num["adder"])) {
+        pairs.push([adder, num["adder"]])
+      }
+    }
+    return pairs.slice(0, pairs.length/2)
+  }
