11ef239248f24877c98c82d00562ce5d0e123f33
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-07-05 00:03:09 -0400

    Implement brute force solution

---
+++ index.js
@@ -0,0 +1,16 @@
+function bruteTwoForceSum(array, sum){
+    let matchingPairs = []
+    let num1
+
+    while (array.length > 0){
+        // pop() would be faster but test is hard-coded to expect shift() :(
+        num1 = array.shift()
+        for (let num2 of array){
+            if (num1 + num2 === sum){
+                matchingPairs.push([num1, num2])
+            }
+        }
+    }
+
+    return matchingPairs
+}
