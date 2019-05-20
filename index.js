1d2857d976baa40625f6019a9b7c8a3cd75d2d26
rcrdlbl <bluh@outlook.com>
2019-05-20 03:03:49 -0400

    Done.

--- index.js
+++ index.js
@@ -25,17 +25,3 @@ function binarySearchTwoSum(array, sum) {
           }
           return results;
 }
-
-function hashTwoSum(array, sum) {
-  let hashMap = {},
-      results = []
-
-        for (let i = 0; i < array.length; i++){
-            if (hashMap[array[i]]){
-                results.push([hashMap[array[i]], array[i]])
-            }else{
-                hashMap[sum - array[i]] = array[i];
-            }
-          }
-          return results;
-}
