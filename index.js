cc2c1e3807f513466f51d839c078774c240117f4
Wali Rahman <wali.rahman89@yahoo.com>
2019-06-30 18:58:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function hashTwoSum(arr, target) {
+    let numObject = {};
+    for (var i = 0; i < arr.length; i++) {
+        let thisNum = arr[i];
+        numObject[thisNum] = i;
+    }
+    for (var i = 0; i < arr.length; i++) {
+        let diff = target - arr[i];
+        if (numObject.hasOwnProperty(diff) && numObject[diff] !== i) {
+            return [i, numObject[diff]];
+        }
+    }
+}
