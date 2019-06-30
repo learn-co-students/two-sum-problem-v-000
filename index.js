680668af3706624fc847214acf9a1f1a98a7d67a
Wali Rahman <wali.rahman89@yahoo.com>
2019-06-30 19:15:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+var bruteForceTwoSum = function (nums, target) {
+    var result = [];
+
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                result.push([nums[i],nums[j]]);
+
+            }
+        }
+    }
+    return result;
+}
+
+
 function hashTwoSum(arr, target) {
     let numObject = {};
     for (var i = 0; i < arr.length; i++) {
