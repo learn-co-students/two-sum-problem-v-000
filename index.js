0aac3ec458de0a82a8aea3d93bc1d68891f58069
Wali Rahman <wali.rahman89@yahoo.com>
2019-07-01 01:25:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var bruteForceTwoSum = function (arr, target) {
+function bruteForceTwoSum(array, target) {
     var result = [];
 
-    for (var i = 0; i < nums.length; i++) {
-        for (var j = i + 1; j < nums.length; j++) {
-            if (arr[i] + arr[j] === target) {
-                result.push([arr[i],arr[j]]);
+    for (var i = 0; i < array.length; i++) {
+        for (var j = i + 1; j < array.length; j++) {
+            if (array[i] + array[j] === target) {
+                result.push([array[i],array[j]]);
 
             }
         }
@@ -12,19 +12,40 @@ var bruteForceTwoSum = function (arr, target) {
     return result;
 }
 
+function binarySearchTwoSum(array, sum) {
+  let sortedArray = array.sort((a, b) => a - b);
+  let sums = [];
+  let startNum;
+  let missingNum;
+  let startInd;
+  let filteredArray = sortedArray;
 
-function hashTwoSum(arr, target) {
-    let numObject = {};
-    var result = [];
-    for (var i = 0; i < arr.length; i++) {
-        let thisNum = arr[i];
-        numObject[thisNum] = i;
+  while (filteredArray.length > 1) {
+    startInd = Math.floor(filteredArray.length/2);
+    startNum = filteredArray[startInd];
+    filteredArray.splice(startInd, 1);
+    missingNum = sum - startNum;
+    if (binaryMatch(filteredArray, missingNum)) {
+      sums.push([missingNum, startNum]);
+      filteredArray = filteredArray.filter(el => el != missingNum);
     }
-    for (var i = 0; i < arr.length; i++) {
-        let diff = target - arr[i];
-        if (numObject.hasOwnProperty(diff) && numObject[diff] !== i) {
-            result.push([arr[i], diff]);
-        }
+  }
+  return sums;
+}
+
+function hashTwoSum(array, sum) {
+  let sums = [];
+  let hashTable = {};
+
+  for (let i = 0; i < array.length; i++) {
+    let sumMinusElement = sum - array[i];
+    //check if this number exists in the hash table
+    //if yes, we found a pair of numbers that add upto sum
+    if (hashTable[sumMinusElement.toString()] !== undefined) {
+      sums.push([sumMinusElement, array[i]])
     }
-    return result;
+    // add the current number to the hash table
+    hashTable[array[i].toString()] = array[i];
+  }
+  return sums;
 }
